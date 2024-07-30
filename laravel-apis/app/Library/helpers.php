ea60ad1d09b9148dbb4bca6525fbbdb70eb14e69
Mohamed Mostafa <mokammel0000@gmail.com>
2024-07-30 03:06:22 +0300

    adding apiResource global function

---
+++ laravel-apis/app/Library/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+function apiResponse($status, $message, $data = null)
+{
+    $response = [
+        'status' => $status,
+        'message' => $message,
+        'data' => $data,
+    ];
+    return response()->json($response);
+}
